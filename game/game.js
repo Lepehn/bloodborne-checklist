c8cd797610f29bcd097eb49706f24da7db1acaf7
Lee <126716094+Lepehn@users.noreply.github.com>
2026-01-04 14:19:14 -0600

    Update game.js

--- game/game.js
+++ game/game.js
@@ -3,6 +3,14 @@ import { STATUSES, COMPLETED_STATUSES } from "./statuses.js";
 import { CATEGORIES } from "./categories.js";
 
 import { bosses } from "./data/bosses.js";
+import { trickWeapons } from "./data/trickWeapons.js";
+import { sideArms } from "./data/sideArms.js";
+import { pthumeruChalices } from "./data/pthumeruChalices.js";
+import { hunterTools } from "./data/hunterTools.js";
+import { dlcTrickWeapons } from "./data/dlcTrickWeapons.js";
+import { dlcSideArms } from "./data/dlcSideArms.js";
+import { dlcHunterTools } from "./data/dlcHunterTools.js";
+
 
 console.log("GAME index loaded");
 
@@ -12,6 +20,14 @@ export const GAME = {
   completedStatuses: COMPLETED_STATUSES,
   categories: CATEGORIES,
   data: {
-    bosses
+    bosses,
+    trickWeapons,
+    sideArms,
+    pthumeruChalices,
+    hunterTools,
+    dlcTrickWeapons,
+    dlcSideArms,
+    dlcHunterTools
   }
+
 };
